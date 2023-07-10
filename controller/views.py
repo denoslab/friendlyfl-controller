c5d977c904f20af56fd12c00fff0aa0717e71486
Steve Drew <steve.drew@ucalgary.ca>
2023-07-09 18:59:26 -0600

    FL-8 Controller should register Site to RS upon start
    - Create initial Django template and preliminary work.

---
+++ controller/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+
+# Create your views here.
+def index(request):
+    class Question:
+
+        def __init__(self):
+            self.id = 0
+            self.question_test = ''
+
+    q1 = Question()
+    q2 = Question()
+    q1.id = 1
+    q1.question_text = "Text1"
+    q2.id = 2
+    q2.question_text = "Text2"
+
+    latest_question_list = [q1, q2]
+    template = loader.get_template("controller/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def list_projects(request):
+    response = requests.get('http://http://localhost:8000/projects/')
+    # convert reponse data into json
+    users = response.json()
+    print(users)
+    return HttpResponse("Users")
