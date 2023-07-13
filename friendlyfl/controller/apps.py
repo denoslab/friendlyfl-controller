ef19d2b8116de40b6fddf0dfb3187992fdc86b84
Meng Zhai <meng.zhai@cyberx.com>
2023-07-13 00:26:39 -0400

    Add poetry and re-origanize project structure

--- friendlyfl/controller/apps.py
+++ friendlyfl/controller/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ControllerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'controller'
+    name = 'friendlyfl.controller'
