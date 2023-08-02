cbb729d4c05862695f7691432d242045f56d43e5
lc <cailei0625@163.com>
2023-08-01 23:12:01 -0400

    [FL-47] enable runs detail page

--- friendlyfl/controller/urls.py
+++ friendlyfl/controller/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("projects/join/", views.project_join),
     path("projects/leave/", views.project_leave),
     path("runs/<int:run_id>/", views.run_detail),
+    path("runs/detail/<int:batch>/<int:project_id>/<int:site_id>/", views.run_detail),
 ]
